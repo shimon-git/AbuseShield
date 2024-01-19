38b49470e96bcb640ccbdbdd1004c45b20f3dcd3
shimon-git <shimon0584064942@gmail.com>
2024-01-19 06:02:54 +0200

    Updating flags

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -3,158 +3,117 @@ package config
 import (
 	"flag"
 	"fmt"
-	"net"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
 
-	"github.com/badoux/checkmail"
-	"github.com/nyaruka/phonenumbers"
-	e "github.com/shimon-git/AbuseShield/internal/errors"
-	"github.com/shimon-git/AbuseShield/internal/helpers"
+	abuseipdb "github.com/shimon-git/AbuseShield/internal/abuse-IP-DB"
+	"github.com/shimon-git/AbuseShield/internal/cpanel"
+	"github.com/shimon-git/AbuseShield/internal/csf"
+	"github.com/shimon-git/AbuseShield/internal/sophos"
 )
 
 type Flags struct {
 	IPFilePath string
-	CSF        bool
-	APIKey     string
 	Config     string
 	Mode       string
 	Email      string
 	SMS        string
+	Sophos     sophos.Sophos
+	Cpanel     cpanel.Cpanel
+	CSF        csf.CSF
+	AbuseDBIP  abuseipdb.AbuseDBIP
+	Global     GlobalConfigurations
 }
+
+const (
+	DEFAULT_SOPHOS_PORT = 4444
+	DEFAULT_SOPHOS_USER = "admin"
+	DEFAULT_INTERVAL    = 3
+	DEFAULT_LIMIT       = 0
+	DEFAULT_CSF_FILE    = "/etc/csf/csf.deny"
+	DEFAULT_CSF_BACKUP  = "/tmp/csf_backup.deny"
+	DEFAULT_RESULTS     = "./abuse_db_ip_results.txt"
+	DEFAULT_IPV4        = true
+	DEFAULT_IPV6        = false
+
+	INTERVAL_FORMAT = "%s %d %s"
+)
+
+var (
+	tempApiKeys string
+	tempUsers   string
+)
 
 func getFlags() Flags {
 	var f Flags
+
+	f.GetFilesFlags()
+	f.GetEmailAndSMSFlags()
+	f.GetModeFlags()
+	f.getSophosFlags()
+	f.getCpanelAndCpanelFlags()
+	f.GetAbuseDBFlags()
+	f.getGlobalFlags()
+
+	flag.Parse()
+	return f
+}
+
+func (f Flags) getSophosFlags() {
+	flag.StringVar(&f.Sophos.Host, "host", "", "Sophos fw host IP address")
+	flag.IntVar(&f.Sophos.Port, "port", DEFAULT_SOPHOS_PORT, fmt.Sprintf("%s %d", "Sophos fw port - default port is", DEFAULT_SOPHOS_PORT))
+	flag.StringVar(&f.Sophos.User, "user", DEFAULT_SOPHOS_USER, fmt.Sprintf("%s %s", "Username to connect to sophos fw - default is", DEFAULT_SOPHOS_USER))
+	flag.StringVar(&f.Sophos.Password, "password", "", "Password to connect to sophos fw")
+	flag.IntVar(&f.Sophos.Interval, "sophos-interval", DEFAULT_INTERVAL, fmt.Sprintf(INTERVAL_FORMAT, "Interval between API requests for avoiding overload on the sophos fw server - default is", DEFAULT_INTERVAL, "seconds"))
+	flag.StringVar(&f.Sophos.GroupName, "group-name", "", "Group name to add the ip addresses in sophos(new group will created if the group not exits)")
+	flag.StringVar(&f.Sophos.Comment, "comment", "", "Comment to set for each new object that will created on the sophos fw server")
+
+	flag.StringVar(&f.Sophos.Host, "h", "", "Alias for --host")
+	flag.IntVar(&f.Sophos.Port, "p", DEFAULT_SOPHOS_PORT, "Alias for --port")
+	flag.StringVar(&f.Sophos.User, "U", "", "Alias for --user")
+	flag.StringVar(&f.Sophos.Password, "P", "", "Alias for --password")
+	flag.StringVar(&f.Sophos.GroupName, "g", "", "Alias for --group-name")
+	flag.StringVar(&f.Sophos.Comment, "C", "", "Alias for --comment")
+}
+
+func (f Flags) getCpanelAndCpanelFlags() {
+	flag.StringVar(&tempUsers, "cpanel-users", "", "Cpanel users to collect the logs and check for abuse")
+	flag.StringVar(&f.CSF.CSFFile, "csf-file", DEFAULT_CSF_FILE, fmt.Sprintf("%s %s", "Path to csf.deny file - default", DEFAULT_CSF_FILE))
+	flag.StringVar(&f.CSF.Backup, "csf-backup", DEFAULT_CSF_BACKUP, fmt.Sprintf("%s %s", "Path to csf backup file(in case csf backup file already exist then it will be recreated) - default", DEFAULT_CSF_BACKUP))
+}
+func (f Flags) getGlobalFlags() {
+	flag.BoolVar(&f.Global.Ipv4, "ipv4", DEFAULT_IPV4, fmt.Sprintf("%s %v", "Check ipv4(if is set to false ipv4 addresses will not be checked) - default is", DEFAULT_IPV4))
+	flag.BoolVar(&f.Global.Ipv6, "ipv6", DEFAULT_IPV6, fmt.Sprintf("%s %v", "Check ipv6(if is set to true ipv6 addresses will be checked) - default is", DEFAULT_IPV6))
+	flag.IntVar(&f.Global.Interval, "interval", DEFAULT_INTERVAL, fmt.Sprintf(INTERVAL_FORMAT, "Global interval between API requests to abusedb-ip or sophos fw - default is", DEFAULT_INTERVAL, "seconds"))
+	flag.IntVar(&f.Global.Interval, "i", DEFAULT_INTERVAL, "Alias for --interval")
+}
+
+func (f Flags) GetAbuseDBFlags() {
+	flag.IntVar(&f.AbuseDBIP.Limit, "limit", DEFAULT_LIMIT, "IP limit to check(limit can be set to check max number of ip addresses)")
+	flag.IntVar(&f.AbuseDBIP.Interval, "abusedb-interval", DEFAULT_INTERVAL, fmt.Sprintf("%s %d %s", "Interval between API requests to not be blocked by yhe abuse-db-ip - default is", DEFAULT_INTERVAL, "seconds"))
+	flag.StringVar(&f.AbuseDBIP.ResultsFile, "results", DEFAULT_RESULTS, fmt.Sprintf("%s %s", "Path to the results file of abuse-db-ip - default is", DEFAULT_RESULTS))
+	flag.StringVar(&tempApiKeys, "api-keys", "", "API keys to authenticate to abuse-db-ip")
+}
+
+func (f Flags) GetFilesFlags() {
 	flag.StringVar(&f.IPFilePath, "ip-file", "", "Path to the IP file to check")
-	flag.StringVar(&f.IPFilePath, "i", "", "Alias for --ip-file")
-
-	flag.BoolVar(&f.CSF, "csf", false, "Enable CSF integration for automating blocking of malicious IPs")
-
-	flag.StringVar(&f.APIKey, "api-key", "", "Set API key to use")
+	flag.StringVar(&f.IPFilePath, "I", "", "Alias for --ip-file")
 
 	flag.StringVar(&f.Config, "config", "", "Path to config file")
 	flag.StringVar(&f.Config, "c", "", "Alias for --config")
+}
 
+func (f Flags) GetModeFlags() {
 	flag.StringVar(&f.Mode, "mode", "a", "Enable modes(e.g, s(sophos),a(abuseDBIP),cp(Cpanel))")
 	flag.StringVar(&f.Mode, "m", "", "Alias for --mode")
+}
 
+func (f Flags) GetEmailAndSMSFlags() {
 	flag.StringVar(&f.Email, "email", "", "Send an email to the provided address when finished")
 	flag.StringVar(&f.SMS, "sms", "", "Send SMS message to the provided phone number when finished")
-	flag.Parse()
+}
 
+/*
 	// Check ip file path has been given
 	if f.IPFilePath == "" && f.Config == "" || f.Mode == "" {
 		fmt.Printf("Usage: %s --ip-file [ip-file-to-check]\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
-	}
-
-	return f
-}
-
-// isValidIPFile - validate the ip file is exist and the format is valid
-func (f Flags) isValidIPFile() (bool, error) {
-	var wg sync.WaitGroup
-	var err error
-
-	// check the ip file exist
-	_, err = os.Stat(f.IPFilePath)
-	if err != nil {
-		return false, fmt.Errorf("%s", e.MakeErr(fmt.Sprintf("%s: %s", e.RETRIEVE_FILE_INFO_ERR, f.IPFilePath), err))
-	}
-
-	// create a data channel
-	dataChan := make(chan string, 10)
-	// add one job to the wait group
-	wg.Add(1)
-
-	// start goroutine to validate the ip file format
-	go ipFormatValidation(dataChan, &wg, filepath.Base(f.IPFilePath), &err)
-	// start goroutine to read the ip file
-	go helpers.IPFileReader(f.IPFilePath, dataChan)
-	// wait the job(the goroutine job will ended)
-	wg.Wait()
-	// check for errors(in case of ip file format error the goroutine ipFormatValidation will set an error)
-	if err != nil {
-		return false, err
-	}
-	// return the results(ip file and ip format is ok)
-	return true, nil
-}
-
-// isValidIP - validate if the given ip is a valid ip
-func isValidIP(ip string) bool {
-	// parse the ip(return true if the ip is valid)
-	parsedIP := net.ParseIP(ip)
-	// return true if the ip is valid and normal(e.g. not special case such as: 0.0.0.0 ,etc)
-	return parsedIP != nil && parsedIP.IsGlobalUnicast()
-}
-
-// isValidPhoneNumber - validate the given phone number
-func (f Flags) isValidPhoneNumber() (bool, error) {
-	// Parse the phone number "ZZ" for unknown region
-	phone, err := phonenumbers.Parse(f.SMS, "ZZ")
-	// Check for errors
-	if err != nil {
-		return false, e.MakeErr(fmt.Sprintf("%s - %s", e.INVALID_PHONE_NUMBER, f.SMS), err)
-	}
-	// get the country code
-	countryCode := phonenumbers.GetRegionCodeForNumber(phone)
-	// return if the phone number is valid or not
-	return phonenumbers.IsValidNumberForRegion(phone, countryCode), nil
-}
-
-// isValidEmail - validate the given email address
-func (f Flags) isValidEmail() (bool, error) {
-	// validate the mx record of the domain
-	if err := checkmail.ValidateMX(f.Email); err != nil {
-		return false, e.MakeErr(nil, err)
-	}
-	// validate the email format
-	if err := checkmail.ValidateFormat(f.Email); err != nil {
-		return false, e.MakeErr(nil, err)
-	}
-	// return the results
-	return true, nil
-}
-
-// isValidMode - check if the provided mode is valid
-func (f Flags) isValidMode() bool {
-	// possible modes - (cp - cpanel mode),(a - abuseDBIP mode), (s - sophos mode)
-	validModes := map[string]bool{"cp": true, "a": true, "s": true}
-	// split the modes(in case of multiply modes)
-	modes := strings.Split(f.Mode, ",")
-	// loop over the given modes and validate each mode
-	for _, m := range modes {
-		if _, ok := validModes[m]; !ok {
-			return false
-		}
-	}
-	// return the results
-	return true
-}
-
-/*
-* ipFormatValidation - validate the ip format of the provided ip file
-* params:
-* c - channel to collect the ip addresses from the reader
-* wg - reference to the wait group for sync with the parent function
-* fname - in case of error pass the file name for informative error message
-* err - reference to the error to set the error message
- */
-func ipFormatValidation(c chan string, wg *sync.WaitGroup, fname string, err *error) {
-	// initial a counter
-	var counter int
-	// loop over the ip's provided by the channel
-	for ip := range c {
-		// increase the counter
-		counter++
-		// check if the ip is valid
-		if !isValidIP(ip) {
-			*err = e.MakeErr(nil, fmt.Errorf("%s - ip: %s- file: %s line: %s", e.IP_IS_NOT_VALID, ip, fname, counter))
-		}
-	}
-	// done the job
-	wg.Done()
-}
+	} */
--- internal/csf/csf.go
+++ internal/csf/csf.go
@@ -4,6 +4,6 @@ type CSF struct {
 	Enable  bool   `yaml:"enable"`
 	Ipv6    bool   `yaml:"ipv6"`
 	Ipv4    bool   `yaml:"ipv4"`
-	Backup  bool   `yaml:"backup"`
+	Backup  string `yaml:"backup"`
 	CSFFile string `yaml:"csf_file"`
 }
